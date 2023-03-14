4da0e1a0012082021880c339d79f9c99852141dc
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-14 15:39:17 -0300

    -

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-const iconMenu = document.querySelector(".fa-bars");
+const iconMenu = document.querySelector(".iconBar");
 
-iconMenu.addEventListener('mouseover',e => {
+iconMenu.addEventListener('click',e => {
+    if(iconMenu.classList.contains("fa-bars")){
+        iconMenu.classList.remove("fa-bars");
+        iconMenu.classList.add("fa-xmark");
+    }
+    else if(iconMenu.classList.contains("fa-xmark")){
+        iconMenu.classList.remove("fa-xmark");
+        iconMenu.classList.add("fa-bars");
+    }
 });
