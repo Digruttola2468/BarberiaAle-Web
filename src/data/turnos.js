4a1c38f363a2a6b25b6903a23aed91e90fa01996
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-15 14:46:06 -0300

    user and menu modify

---
+++ src/data/turnos.js
@@ -0,0 +1,8 @@
+export const turnos = [
+    {
+        id: 0,
+        nombre: "Ivan DI Gruttola",
+        fecha: "18/03/2022",
+        horario: "19:30"
+    }
+]
--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-const iconMenu = document.querySelector(".iconBar");
+const iconMenu = document.querySelector(".fa-bars");
+const divIconMenu = document.querySelector(".items-barberia");
 
 iconMenu.addEventListener('click',e => {
     if(iconMenu.classList.contains("fa-bars")){
         iconMenu.classList.remove("fa-bars");
         iconMenu.classList.add("fa-xmark");
+        divIconMenu.classList.add("showMenu");
     }
     else if(iconMenu.classList.contains("fa-xmark")){
         iconMenu.classList.remove("fa-xmark");
         iconMenu.classList.add("fa-bars");
+        divIconMenu.classList.remove("showMenu");
     }
+});
+
+const clickUserIcon = document.querySelector(".iconUser-Container");
+
+clickUserIcon.addEventListener('click', e => {
+    window.location.href = "./user.html";
 });
