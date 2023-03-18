858ac3c5f37b99df9d03e636e7715a38e23bfc87
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-17 23:54:01 -0300

    optimize

---
+++ src/dateFechaHora.js
@@ -0,0 +1,38 @@
+import { listHorarios } from "./data/disponibilidadPeluquero.js";
+
+export function getHorarioDay(abre, cierra) {
+  return listHorarios.slice(
+    listHorarios.indexOf(abre),
+    listHorarios.indexOf(cierra) + 1
+  );
+}
+
+export function getStringDate() {
+  const fechaActual = new Date();
+  let dia = fechaActual.getDate();
+  let mounth = fechaActual.getMonth() + 1;
+  let year = fechaActual.getFullYear();
+  if (mounth < 10) mounth = `0${mounth}`;
+  return `${year}-${mounth}-${dia}`;
+}
+
+export const compareDates = (d1, d2) => {
+  let date1 = new Date(d1).getTime();
+  let date2 = new Date(d2).getTime();
+
+  if (date1 < date2) return false;
+  else if (date1 > date2) return true;
+  else return null;
+};
+
+export function getStringFechaHoraFormateado() {
+  let horaActual = new Date().getHours().toString();
+  let minutosActual = new Date().getMinutes().toString();
+  if (minutosActual > 0 && minutosActual < 30) {
+    minutosActual = "30";
+  } else if (minutosActual > 30) {
+    minutosActual = "00";
+    horaActual++;
+  }
+  return `${horaActual}:${minutosActual}`;
+}
--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
 import { showMessage } from "./showMessage.js";
 import { listTurnos } from "./data/turnos.js";
-import { infoPeluquero, listHorarios } from "./data/disponibilidadPeluquero.js";
+import { infoPeluquero } from "./data/disponibilidadPeluquero.js";
+import {
+  compareDates,
+  getHorarioDay,
+  getStringDate,
+  getStringFechaHoraFormateado,
+} from "./dateFechaHora.js";
 
 const priceCorte = document.querySelector("#priceCorte");
-priceCorte.innerHTML = ` ${infoPeluquero.precio}`;
-
 const containerTurnos = document.querySelector(".main-container-turnos");
-
 const inputDate = document.querySelector("#inputDate");
-inputDate.setAttribute("min", getStringDate());
 const selectTurnosButton = document.querySelector(".selectTurnosButton");
+const btnConfirmar = document.querySelector("#btnConfirmar");
 
 const descripcion = document.createElement("p");
+
+priceCorte.innerHTML = ` ${infoPeluquero.precio}`;
+inputDate.setAttribute("min", getStringDate());
+
 let valorButon = "";
+
 inputDate.addEventListener("change", (evt) => {
   selectTurnosButton.innerHTML = "";
   valorButon = "";
+
   const getInputDate = evt.target.value;
-  if (compareDates(getStringDate(), getInputDate) != true) {
+
+  if (!compareDates(getStringDate(), getInputDate)) {
     const date = new Date(getInputDate);
     let diaAbre;
     let diaCierra;
@@ -65,17 +75,9 @@ inputDate.addEventListener("change", (evt) => {
     });
 
     if (compareDates(getStringDate(), getInputDate) == null) {
-      let horaActual = new Date().getHours().toString();
-      let minutosActual = new Date().getMinutes().toString();
-      if (minutosActual > 0 && minutosActual < 30) {
-        minutosActual = "30";
-      } else if (minutosActual > 30) {
-        minutosActual = "00";
-        horaActual++;
-      }
-      const timeActual = `${horaActual}:${minutosActual}`;
-
-      const d1 = new Date(`${getStringDate()}T${timeActual}`);
+      const d1 = new Date(
+        `${getStringDate()}T${getStringFechaHoraFormateado()}`
+      );
       array.map((time, index) => {
         const d2 = new Date(`${getStringDate()}T${time}`);
         if (d1 > d2) {
@@ -88,72 +90,43 @@ inputDate.addEventListener("change", (evt) => {
       array.map((elemen) => {
         const createButton = document.createElement("button");
         createButton.innerHTML = elemen;
-        createButton.classList.add("btnHorario");
         createButton.addEventListener("click", (e) => {
           valorButon = elemen;
           descripcion.innerHTML = `Turno para el ${getInputDate} a las ${elemen}`;
         });
         selectTurnosButton.appendChild(createButton);
       });
-    } else {
-      const createParrafo = document.createElement("p");
-      createParrafo.innerHTML = "NO HAY TURNOS DISPONIBLES";
-      selectTurnosButton.appendChild(createParrafo);
-    }
+    } else showMessage("No Abrimos", "error");
+
     descripcion.innerHTML = `Turno para el ${getInputDate} a las ${valorButon}`;
     containerTurnos.appendChild(descripcion);
   }
 });
 
-const btnConfirmar = document.querySelector("#btnConfirmar");
 btnConfirmar.addEventListener("click", (e) => {
-  console.log(inputDate.value);
-  console.log(valorButon);
-  if (inputDate.value != "" && valorButon != ""){
-    if (confirm(`El turno que elegiste es el ${inputDate.value} a las ${valorButon}, seleccione aceptar para confirmar`)) {
-      
-
-      listTurnos.map(turno => {
-        if(turno.nombre == "Ivan DI Gruttola"){
+  if (inputDate.value != "" && valorButon != "") {
+    if (
+      confirm(
+        `El turno que elegiste es el ${inputDate.value} a las ${valorButon}, seleccione aceptar para confirmar`
+      )
+    ) {
+      listTurnos.map((turno) => {
+        if (turno.nombre == "Ivan DI Gruttola") {
           turno.turno.fecha = inputDate.value;
           turno.turno.horario = valorButon;
         }
-      })
-      console.log(listTurnos);
-      showMessage("Guardado","success");
+      });
+      showMessage("Guardado", "success");
 
-      valorButon = "";
-      inputDate.value = "";
-      selectTurnosButton.innerHTML = "";
-      descripcion.innerHTML = "";
-    } else {
-      showMessage("Cancelado","error")
-    }
+      vaciarCampos();
+
+    } else showMessage("Cancelado", "error");
   }
 });
-console.log(valorButon);
 
-function getHorarioDay(abre, cierra) {
-  return listHorarios.slice(
-    listHorarios.indexOf(abre),
-    listHorarios.indexOf(cierra) + 1
-  );
-}
-
-function getStringDate() {
-  const fechaActual = new Date();
-  let dia = fechaActual.getDate();
-  let mounth = fechaActual.getMonth() + 1;
-  let year = fechaActual.getFullYear();
-  if (mounth < 10) mounth = `0${mounth}`;
-  return `${year}-${mounth}-${dia}`;
-}
-
-const compareDates = (d1, d2) => {
-  let date1 = new Date(d1).getTime();
-  let date2 = new Date(d2).getTime();
-
-  if (date1 < date2) return false;
-  else if (date1 > date2) return true;
-  else return null;
+const vaciarCampos = () => {
+  valorButon = "";
+  inputDate.value = "";
+  selectTurnosButton.innerHTML = "";
+  descripcion.innerHTML = "";
 };
