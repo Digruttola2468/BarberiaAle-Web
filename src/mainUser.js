3ff407b0632806750f5625cc29f6f35e433f4150
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2023-03-20 15:17:33 -0300

    sign in

---
+++ src/mainUser.js
@@ -0,0 +1,6 @@
+const NombreUserH1 = document.querySelector("#NombreUserH1");
+const EmailUserP = document.querySelector("#EmailUserP");
+const PhoneUserP = document.querySelector("#PhoneUserP");
+const FechaTurnoP = document.querySelector("#FechaTurnoP");
+const HorarioTurnoP = document.querySelector("#HorarioTurnoP");
+
